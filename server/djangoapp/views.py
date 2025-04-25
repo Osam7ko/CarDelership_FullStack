56e7772db3a80d43e73c054e62fd200bab1ae663
osam7ko <osam7ko@gmail.com>
2025-04-25 12:32:48 -0400

    Implement get_cars view, fix populate script with dealer_id, and ensure proper data loading

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,8 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .models import CarMake, CarModel
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -94,3 +95,19 @@ def registration_request(request):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+@csrf_exempt
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(f"Current CarMake count: {count}")
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+
+    if not car_models.exists():
+        print("No car models found even after initiate.")
+
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    
+    return JsonResponse({"CarModels": cars})
